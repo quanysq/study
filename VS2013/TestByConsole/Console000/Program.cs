a96c8b95bb1bcef763990da7c807a9c3aaf4b352
quanysq <51398798@qq.com>
2016-12-20 16:04:13 +0800

    TestByConsole

--- VS2013/TestByConsole/Console000/Program.cs
+++ VS2013/TestByConsole/Console000/Program.cs
@@ -12,18 +12,8 @@ namespace Console000
   {
     static void Main(string[] args)
     {
-      //Class0.Execute(args);
-      //Class1.Execute(args);
-      //Class2.Execute(args);
-      //Class3.Execute(args);
-      //Console.WriteLine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase);
-      //Class4.Execute(args);
-      //Class6.Execute(args);
-      
-      Console.ReadKey();
+      Console.WriteLine("Hello, world.");
     }
-
-    
   }
 
 
