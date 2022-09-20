9bb4e65edf18de603a0118f7559ad3b909b84f02
jacky <yangshuquan@andogd.partner.onmschina.cn>
2022-09-20 17:59:31 +0800

    asp.net core webapi

--- TestProject_VS2022/NetCore/webapi/TodoApi/TodoApi/Program.cs
+++ TestProject_VS2022/NetCore/webapi/TodoApi/TodoApi/Program.cs
@@ -1,19 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// 注入数据库上下文服务到依赖注入容器
+// 指定数据库上下文将使用内存中的数据库
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
